c4cc354af9f84b375910086fded6adb60fff7dac
timothy-tolley <timothytolley@outlook.com>
2018-09-11 15:19:06 +1200

    fixed logic for graphing - added active state

--- client/components/AnalysisOne.jsx
+++ client/components/AnalysisOne.jsx
@@ -8,21 +8,26 @@ class AnalysisOne extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      active: false,
       GBgraphData: [],
       GBP10: null,
       GBP50: null,
       GBP90: null,
-      GBP92: null,
+      GBP99: null,
+      GBMax: null,
       ICgraphData: [],
       ICP10: null,
       ICP50: null,
       ICP90: null,
-      ICP92: null,
+      ICP99: null,
+      ICMax: null,
       GCgraphData: [],
       GCP10: null,
       GCP50: null,
       GCP90: null,
-      GCP92: null
+      GCP99: null,
+      GCMax: null,
+      GBGCMax: null
     }
   }
 
@@ -40,25 +45,25 @@ class AnalysisOne extends React.Component {
         // GROUP BENEFIT
         // array for group benefit calculations
         let GBArray = [
-          res.body.gbd[0].pessimistic,
-          res.body.gbd[0].optimistic,
-          res.body.gbd[0].likely
+          Math.log(res.body.gbd[0].pessimistic),
+          Math.log(res.body.gbd[0].optimistic),
+          Math.log(res.body.gbd[0].likely)
         ]
         // mean + stdev
         let gbMean = jStat.mean(GBArray)
         let gbStd = jStat.stdev(GBArray, true)
-        let gbMeanMathLog = Math.log(gbMean)
-        let gbStdMathLog = Math.log(gbStd)
+        // let gbMeanMathLog = Math.log(gbMean)
+        // let gbStdMathLog = Math.log(gbStd)
         // p values
-        let GBP10 = jStat.lognormal.inv(0.1, gbMeanMathLog, gbStdMathLog)
-        let GBP50 = jStat.lognormal.inv(0.5, gbMeanMathLog, gbStdMathLog)
-        let GBP90 = jStat.lognormal.inv(0.9, gbMeanMathLog, gbStdMathLog)
-        let GBP92 = jStat.lognormal.inv(0.92, gbMeanMathLog, gbStdMathLog)
+        let GBP10 = jStat.lognormal.inv(0.1, gbMean, gbStd)
+        let GBP50 = jStat.lognormal.inv(0.5, gbMean, gbStd)
+        let GBP90 = jStat.lognormal.inv(0.9, gbMean, gbStd)
+        let GBP99 = jStat.lognormal.inv(0.99, gbMean, gbStd)
         // graph results
-        let GBxVals = _.range(0, GBP92, (GBP92 / 10))
+        let GBxVals = _.range(0, GBP99, (GBP99 / 100))
         let GByVals = []
         let GBgraphData = GBxVals.map(xVal => {
-          let GByVal = jStat.lognormal.pdf(xVal, gbMeanMathLog, gbStdMathLog)
+          let GByVal = jStat.lognormal.pdf(xVal, gbMean, gbStd)
           GByVals.push(GByVal)
           return {x: xVal, y: GByVal}
         })
@@ -67,63 +72,68 @@ class AnalysisOne extends React.Component {
         // INDIVIDUAL COST
         // array for ID calculations
         let ICArray = [
-          res.body.icd[0].pessimistic,
-          res.body.icd[0].optimistic,
-          res.body.icd[0].likely
+          Math.log(res.body.icd[0].pessimistic),
+          Math.log(res.body.icd[0].optimistic),
+          Math.log(res.body.icd[0].likely)
         ]
         // mean + stdev
         let ICMean = jStat.mean(ICArray)
         let ICStDev = jStat.stdev(ICArray, true)
-        let ICMeanMathLog = Math.log(ICMean)
-        let ICStDevMathLog = Math.log(ICStDev)
+        // let ICMeanMathLog = Math.log(ICMean)
+        // let ICStDevMathLog = Math.log(ICStDev)
         // p values
-        let ICP10 = jStat.lognormal.inv(0.1, ICMeanMathLog, ICStDevMathLog)
-        let ICP50 = jStat.lognormal.inv(0.5, ICMeanMathLog, ICStDevMathLog)
-        let ICP90 = jStat.lognormal.inv(0.9, ICMeanMathLog, ICStDevMathLog)
-        let ICP99 = jStat.lognormal.inv(0.99, ICMeanMathLog, ICStDevMathLog)
+        let ICP10 = jStat.lognormal.inv(0.1, ICMean, ICStDev)
+        let ICP50 = jStat.lognormal.inv(0.5, ICMean, ICStDev)
+        let ICP90 = jStat.lognormal.inv(0.9, ICMean, ICStDev)
+        let ICP99 = jStat.lognormal.inv(0.99, ICMean, ICStDev)
         // graph results
         let ICxVals = _.range(0, ICP99, (ICP99 / 100))
         let ICyVals = []
         let ICgraphData = ICxVals.map(xVal => {
-          let ICyVal = jStat.lognormal.pdf(xVal, ICMeanMathLog, ICStDevMathLog)
+          let ICyVal = jStat.lognormal.pdf(xVal, ICMean, ICStDev)
           ICyVals.push(ICyVal)
           return {x: xVal, y: ICyVal}
         })
         let ICMax = Math.max(...ICyVals)
+        let GImaxs = [ICMax, GBMax]
+        let GBICmax = Math.max(...GImaxs)
 
         // GROUP COST
         // array for ID calculations
         let GCArray = []
         res.body.gcd.forEach(dataset =>
-          GCArray.push(dataset.pessimistic, dataset.optimistic, dataset.likely)
+          GCArray.push(Math.log(dataset.pessimistic), Math.log(dataset.optimistic), Math.log(dataset.likely))
         )
         // mean + stdev
         let GCMean = jStat.mean(GCArray)
         let GCStDev = jStat.stdev(GCArray, true)
-        let GCMeanMathLog = Math.log(GCMean)
-        let GCStDevMathLog = Math.log(GCStDev)
+        // let GCMeanMathLog = Math.log(GCMean)
+        // let GCStDevMathLog = Math.log(GCStDev)
         // p values
-        let GCP10 = jStat.lognormal.inv(0.1, GCMeanMathLog, GCStDevMathLog)
-        let GCP50 = jStat.lognormal.inv(0.5, GCMeanMathLog, GCStDevMathLog)
-        let GCP90 = jStat.lognormal.inv(0.9, GCMeanMathLog, GCStDevMathLog)
-        let GCP99 = jStat.lognormal.inv(0.99, GCMeanMathLog, GCStDevMathLog)
+        let GCP10 = jStat.lognormal.inv(0.1, GCMean, GCStDev)
+        let GCP50 = jStat.lognormal.inv(0.5, GCMean, GCStDev)
+        let GCP90 = jStat.lognormal.inv(0.9, GCMean, GCStDev)
+        let GCP99 = jStat.lognormal.inv(0.99, GCMean, GCStDev)
         // graph results
         let GCxVals = _.range(0, GCP99, (GCP99 / 100))
         let GCyVals = []
         let GCgraphData = GCxVals.map(xVal => {
-          let GCyVal = jStat.lognormal.pdf(xVal, GCMeanMathLog, GCStDevMathLog)
+          let GCyVal = jStat.lognormal.pdf(xVal, GCMean, GCStDev)
           GCyVals.push(GCyVal)
           return {x: xVal, y: GCyVal}
         })
         let GCMax = Math.max(...GCyVals)
+        let GGmaxs = [GCMax, GBMax]
+        let GBGCmax = Math.max(...GGmaxs)
 
         // add all to state
         this.setState({
+          active: true,
           GBgraphData: GBgraphData,
           GBP10: GBP10,
           GBP50: GBP50,
           GBP90: GBP90,
-          GBP92: GBP92,
+          GBP99: GBP99,
           GBMax: GBMax,
           ICgraphData: ICgraphData,
           ICP10: ICP10,
@@ -131,12 +141,14 @@ class AnalysisOne extends React.Component {
           ICP90: ICP90,
           ICP99: ICP99,
           ICMax: ICMax,
+          GBICMax: GBICmax,
           GCgraphData: GCgraphData,
           GCP10: GCP10,
           GCP50: GCP50,
           GCP90: GCP90,
           GCP99: GCP99,
-          GCMax: GCMax
+          GCMax: GCMax,
+          GBGCMax: GBGCmax
         })
       })
     // eslint-disable-next-line no-console
@@ -150,129 +162,141 @@ class AnalysisOne extends React.Component {
           Analysis One
         </h1>
         <h1 className = 'analysis-text'> Group Benefit vs Individual Cost </h1>
-        <Legend
-          data= {[
-            {
-              key: 'Group Benefit Curve',
-              color: 'orange'
-            }, {
-              key: 'Individual Cost Curve',
-              color: 'blue'
-            }, {
-              key: 'P10',
-              color: 'cyan'
-            }, {
-              key: 'P50',
-              color: 'red'
-            }, {
-              key: 'p90',
-              color: 'purple'
+        {this.state.active && <div>
+          <Legend
+            data= {[
+              {
+                key: 'Group Benefit Curve',
+                color: 'orange'
+              }, {
+                key: 'Individual Cost Curve',
+                color: 'blue'
+              }, {
+                key: 'P10',
+                color: 'cyan'
+              }, {
+                key: 'P50',
+                color: 'red'
+              }, {
+                key: 'p90',
+                color: 'purple'
+              }
+            ]}
+            dataId={'key'}
+            horizontal
+            config = {[
+              {color: 'orange'},
+              {color: 'blue'},
+              {color: 'cyan'},
+              {color: 'red'},
+              {color: 'purple'}
+            ]}
+            styles = {{
+              '.legend': {
+                backgroundColor: '#f9f9f9',
+                borderRadius: '2px',
+                fontSize: '0.8em',
+                marginLeft: '40px',
+                maxWidth: '50%',
+                fontFamily: '"Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif'
+              }
+            }}/>
+          <LineChart
+            data={
+              [
+                this.state.GBgraphData,
+                this.state.ICgraphData,
+                [{x: this.state.GBP10, y: 0}, {x: this.state.GBP10, y: this.state.GBICMax}],
+                [{x: this.state.GBP50, y: 0}, {x: this.state.GBP50, y: this.state.GBICMax}],
+                [{x: this.state.GBP90, y: 0}, {x: this.state.GBP90, y: this.state.GBICMax}]
+              ]
             }
-          ]}
-          dataId={'key'}
-          horizontal
-          config = {[
-            {color: 'orange'},
-            {color: 'blue'},
-            {color: 'cyan'},
-            {color: 'red'},
-            {color: 'purple'}
-          ]}
-          styles = {{
-            '.legend': {
-              backgroundColor: '#f9f9f9',
-              borderRadius: '2px',
-              fontSize: '0.8em',
-              marginLeft: '40px',
-              maxWidth: '50%',
-              fontFamily: '"Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif'
+            width={1000}
+            height={400}
+            margin={{top: 40, right: 5, bottom: 30, left: 100}}
+            axes
+            axisLabels={{x: 'X Axis', y: 'Y Axis'}}
+            interpolate={'cardinal'}
+            grid
+            verticalGrid
+            lineColors={['orange', 'blue', 'cyan', 'red', 'purple']}
+            xTicks={10}
+            yTicks={10}
+          />
+          <h1 className = 'analysis-text'>
+            <p className = 'analysis-text-small' >
+                  p 10 - {this.state.GBP10}
+            </p>
+            <p className = 'analysis-text-small' >
+                  p 50 - {this.state.GBP50}
+            </p>
+            <p className = 'analysis-text-small' >
+                  p 90 - {this.state.GBP90}
+            </p>
+          </h1>
+          <h1 className = 'analysis-text'> Group Benefit vs Group Cost </h1>
+          <Legend
+            data= {[
+              {
+                key: 'Group Benefit Curve',
+                color: 'orange'
+              }, {
+                key: 'Group Cost Curve',
+                color: 'green'
+              }, {
+                key: 'P10',
+                color: 'cyan'
+              }, {
+                key: 'P50',
+                color: 'red'
+              }, {
+                key: 'p90',
+                color: 'purple'
+              }
+            ]}
+            dataId={'key'}
+            horizontal
+            config = {[
+              {color: 'orange'},
+              {color: 'green'},
+              {color: 'cyan'},
+              {color: 'red'},
+              {color: 'purple'}
+            ]}
+            styles = {{
+              '.legend': {
+                backgroundColor: '#f9f9f9',
+                borderRadius: '2px',
+                fontSize: '0.8em',
+                marginLeft: '40px',
+                maxWidth: '50%',
+                fontFamily: '"Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif'
+              }
+            }}/>
+          <LineChart
+            data={
+              [
+                this.state.GBgraphData,
+                this.state.GCgraphData,
+                [{x: this.state.GBP10, y: 0}, {x: this.state.GBP10, y: this.state.GBGCMax}],
+                [{x: this.state.GBP50, y: 0}, {x: this.state.GBP50, y: this.state.GBGCMax}],
+                [{x: this.state.GBP90, y: 0}, {x: this.state.GBP90, y: this.state.GBGCMax}]
+              ]
             }
-          }}/>
-        <LineChart
-          data={
-            [
-              this.state.GBgraphData,
-              this.state.ICgraphData,
-              [{x: this.state.GBP10, y: 0}, {x: this.state.GBP10, y: this.state.GBMax}],
-              [{x: this.state.GBP50, y: 0}, {x: this.state.GBP50, y: this.state.GBMax}],
-              [{x: this.state.GBP90, y: 0}, {x: this.state.GBP90, y: this.state.GBMax}]
-            ]
-          }
-          width={1000}
-          height={400}
-          margin={{top: 40, right: 5, bottom: 30, left: 100}}
-          axes
-          axisLabels={{x: 'X Axis', y: 'Y Axis'}}
-          interpolate={'cardinal'}
-          grid
-          verticalGrid
-          lineColors={['orange', 'blue', 'cyan', 'red', 'purple']}
-          xDomainRange={[0, (this.state.GBP90 + 10000)]}
-          xTicks={10}
-          yTicks={10}
-        />
-        <h1 className = 'analysis-text'> Group Benefit vs Group Cost </h1>
-        <Legend
-          data= {[
-            {
-              key: 'Group Benefit Curve',
-              color: 'orange'
-            }, {
-              key: 'Group Cost Curve',
-              color: 'green'
-            }, {
-              key: 'P10',
-              color: 'cyan'
-            }, {
-              key: 'P50',
-              color: 'red'
-            }, {
-              key: 'p90',
-              color: 'purple'
-            }
-          ]}
-          dataId={'key'}
-          horizontal
-          config = {[
-            {color: 'orange'},
-            {color: 'green'},
-            {color: 'cyan'},
-            {color: 'red'},
-            {color: 'purple'}
-          ]}
-          styles = {{
-            '.legend': {
-              backgroundColor: '#f9f9f9',
-              borderRadius: '2px',
-              fontSize: '0.8em',
-              marginLeft: '40px',
-              maxWidth: '50%',
-              fontFamily: '"Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif'
-            }
-          }}/>
-        <LineChart
-          data={
-            [
-              this.state.GBgraphData,
-              this.state.GCgraphData,
-              [{x: this.state.GBP10, y: 0}, {x: this.state.GBP10, y: this.state.GBMax}],
-              [{x: this.state.GBP50, y: 0}, {x: this.state.GBP50, y: this.state.GBMax}],
-              [{x: this.state.GBP90, y: 0}, {x: this.state.GBP90, y: this.state.GBMax}]
-            ]
-          }
-          width={1000}
-          height={400}
-          margin={{top: 40, right: 5, bottom: 30, left: 100}}
-          axes
-          axisLabels={{x: 'X Axis', y: 'Y Axis'}}
-          interpolate={'cardinal'}
-          grid
-          verticalGrid
-          lineColors={['orange', 'green', 'cyan', 'red', 'purple']}
-          xDomainRange={[0, (this.state.GBP90 + 10000)]}
-          xTicks={10}
-          yTicks={10}
-        />
+            width={1000}
+            height={400}
+            margin={{top: 40, right: 5, bottom: 30, left: 100}}
+            axes
+            axisLabels={{x: 'X Axis', y: 'Y Axis'}}
+            interpolate={'cardinal'}
+            grid
+            verticalGrid
+            lineColors={['orange', 'green', 'cyan', 'red', 'purple']}
+            xTicks={10}
+            yTicks={10}
+          />
+        </div>
+        }
 
         <button className = 'spacer-button' onClick = {() => { location.href = '/element-estimations' }}>
           Next
